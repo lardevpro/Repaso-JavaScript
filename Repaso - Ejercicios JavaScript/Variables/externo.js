a0f3efcc2afa37d844c8011be9e66fac08ea6367
lardevpro <lardevpro@gmail.com>
2025-03-27 21:29:07 +0100

    review exercises variables and operators

---
+++ Repaso - Ejercicios JavaScript/Variables/externo.js
@@ -0,0 +1,38 @@
+//variable implicita que obliga a darle un valor 
+nombre_amigo = "Manuel";// no necesita let, var ni const pero es obligada tener un valor literal al declararse
+
+//variables explicitas que usan var, let o const que se pueden declarar sin darles un valor inicial:
+
+const PI = 3.14; //constante, no se puede modificar su valor, el identificador de escribe en MAYUSCULAS
+
+
+let n1 = 30; // varibale explicita con let
+var nombre_persona = "Juan"; //var no está recomendado
+
+
+//tipos de datos:
+
+//primitivos
+let numero_entero = 10; //número enteros
+
+let texto = "hola"; //texto o cadena de caracteres
+
+let booleano = true; //booleano, puede ser true o false
+
+let nulo = null; //nulo, no tiene valor
+
+let indefinido = undefined; //indefinido, no tiene valor
+
+//simbolos
+
+
+
+
+//objetos
+let objeto = { //objeto, lista de valores con nombre
+    nombre: "Juan",
+    edad: 30,
+    ciudad: "Madrid",
+  }; //objeto, lista de valores con nombre
+
+let array = [1, 2, 3]; //array, lista de valores
